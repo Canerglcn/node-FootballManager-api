0b7dbf7183b1668ba566e5a1ccc891c939ff71b8
Caner <canerglcn@gmail.com>
2020-02-11 21:05:28 +0300

    api/teams/[Get] end point

--- routes/team.js
+++ routes/team.js
@@ -1,9 +1,76 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
+const Team=require('../models/Team');
+
+
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+    const promise=Team.aggregate([
+      {
+          $lookup:{
+            from:'footballers',
+            localField:'_id',
+            foreignField:'team_id',
+            as:'footballers'
+          }
+      },
+      {
+         $unwind:{
+            path:'$footballers',
+            preserveNullAndEmptyArrays:true
+         }
+      },
+      {
+          $group:{
+            _id:{
+                _id:'$_id',
+                name:'$name',
+                history:'$history',
+                foundation_year:'$foundation_year',
+                country:'$country',
+                location:'$location'
+            },
+            footballers:{
+                $push:'$footballers'
+            }
+          }
+      },
+      {
+          $project:{
+              _id:'$_id._id',
+              name:'$_id.name',
+              country:'$_id.country',
+              footballers:'$footballers'
+          }
+      }
+    ]);
+
+
+    promise.then((teams)=>{
+      res.json(teams);
+    }).catch((err)=>{
+      res.json(err);
+    })
 });
+
+router.post('/new', (req, res, next) => {
+
+  const team=new Team(req.body);
+  const promise=team.save();
+
+  promise.then((data)=>{
+    res.json(data);
+  }).catch((err)=>{
+    res.json(err);
+  });
+
+});
+
+
+
+
+
+
+
 
 module.exports = router;
