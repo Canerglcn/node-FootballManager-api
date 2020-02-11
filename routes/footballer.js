0d39fea3b44adea0bbba9442634e71edf892cd30
Caner <canerglcn@gmail.com>
2020-02-11 16:32:15 +0300

    api/footballers/ [POST] end point

--- routes/footballer.js
+++ routes/footballer.js
@@ -16,20 +16,16 @@ router.get('/', (req, res, next) => {
 
 
 router.post('/new', (req, res, next) => {
-    const footballer=new Footballer({
-        name:'caner',
-        surname:'canerovic',
-        position_number: 21,
-        datebirth: '19800810',
-        country:'Turkey',
-        rating:'80'
-    });
-    footballer.save((err,data)=>{
-        if(err)
-            console.log(err);
 
-        res.json(data);
+    const footballer=new Footballer(req.body);
+
+    const promise=footballer.save();
+    promise.then((data)=>{
+    res.json(data);
+    }).catch((err)=>{
+    res.json(err);
     });
+
 });
 
 module.exports = router;
